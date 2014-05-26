031e0e5cc7c625965add3f6421d44d7ea70bedd7
Emil Falk <emph@emph.se>
2014-05-26 17:05:18 +0200

    Fixed unit tests

--- test/gettexter_test.erl
+++ test/gettexter_test.erl
@@ -1,71 +1,73 @@
 %% @doc Uses locale/se/LC_MESSAGES/default.po
 -module(gettexter_test).
 
--define(GETTEXT_DOMAIN, default).
+-define(DOMAIN, default).
 
 -include("shortcuts.hrl").
 
 -include_lib("eunit/include/eunit.hrl").
 
-loaded_test() ->
-    {foreach, fun start_load/0, fun stop/1,
-     [fun dpgettext_loaded/1,
-      fun dnpgettext_loaded/1]}.
+gettext4_noloaded_test_() ->
+    {setup, fun start/0, fun stop/1, fun gettext4_noloaded/1}.
 
-unloaded_test() ->
-    {foreach, fun start/0, fun stop/1,
-     [fun dpgettext_noloaded/1,
-      fun dnpgettext_noloaded/1]}.
+gettext6_noloaded_test_() ->
+    {setup, fun start/0, fun stop/1, fun gettext6_noloaded/1}.
 
-dpgettext_loaded(_) ->
-    [?_assertEqual(<<"Hejsan">>, ?_(<<"Hello">>, <<"se">>)),
-     ?_assertEqual(<<"NoTranslation">>, ?_(<<"NoTranslation">>, <<"se">>)),
-     ?_assertEqual(<<"Tjena">>, ?P_(<<"Context">>, <<"Hello">>, <<"se">>)),
-     ?_assertEqual(<<"NoTranslation">>, ?P_(<<"Context">>, <<"NoTranslation">>, <<"se">>))].
+gettext4_loaded_test_() ->
+    {setup, fun start_load/0, fun stop/1, fun gettext4_loaded/1}.
 
-%% Calls to dpgettext/4 without loaded translations
-dpgettext_noloaded(_) ->
-     [?_assertEqual(<<"Hello">>, ?_(<<"Hello">>, <<"se">>)),
-      ?_assertEqual(<<>>, ?_(<<>>, <<"se">>)),
-      ?_assertEqual(<<"Hello">>, ?P_(<<"Context">>, <<"Hello">>, <<"se">>)),
-      ?_assertEqual(<<>>, ?P_(<<>>, <<>>, <<"se">>)),
+gettext6_loaded_test_() ->
+    {setup, fun start_load/0, fun stop/1, fun gettext6_loaded/1}.
+
+gettext4_loaded(_) ->
+    [?_assertEqual(<<"Hejsan">>, ?_(<<"se">>, <<"Hello">>)),
+     ?_assertEqual(<<"NoTranslation">>, ?_(<<"se">>, <<"NoTranslation">>)),
+     ?_assertEqual(<<"Tjena">>, ?P_(<<"se">>, <<"Context">>, <<"Hello">>)),
+     ?_assertEqual(<<"NoTranslation">>, ?P_(<<"se">>, <<"Context">>, <<"NoTranslation">>))].
+
+gettext4_noloaded(_) ->
+     [?_assertEqual(<<"Hello">>, ?_(<<"se">>, <<"Hello">>)),
+      ?_assertEqual(<<>>, ?_(<<"se">>, <<>>)),
+      ?_assertEqual(<<"Hello">>, ?P_(<<"se">>, <<"Context">>, <<"Hello">>)),
+      ?_assertEqual(<<>>, ?P_(<<"se">>, <<>>, <<>>)),
       ?_assertEqual(<<>>, ?_(<<>>, <<>>)),
       ?_assertEqual(<<>>, ?P_(<<>>, <<>>, <<>>))].
 
-dnpgettext_loaded(_) ->
-    [?_assertEqual(<<"Fisk">>, ?N_(<<"Fish">>, <<"Fishes">>, 1, <<"se">>)),
-     ?_assertEqual(<<"Fiskar">>, ?N_(<<"Fish">>, <<"Fishes">>, 2, <<"se">>)),
-     ?_assertEqual(<<"NoTranslation">>, ?N_(<<"NoTranslation">>, <<"NoTranslations">>, 1, <<"se">>)),
-     ?_assertEqual(<<"NoTranslations">>, ?N_(<<"NoTranslation">>, <<"NoTranslations">>, 2, <<"se">>)),
-     ?_assertEqual(<<"Get">>, ?NP_(<<"Context">>, <<"Goat">>, <<"Goats">>, 1, <<"se">>)),
-     ?_assertEqual(<<"Getter">>, ?NP_(<<"Context">>, <<"Goat">>, <<"Goats">>, 2, <<"se">>)),
-     ?_assertEqual(<<"NoTranslation">>, ?NP_(<<"Context">>, <<"NoTranslation">>, <<"NoTranslations">>, 1, <<"se">>)),
-     ?_assertEqual(<<"NoTranslations">>, ?NP_(<<"Context">>, <<"NoTranslation">>, <<"NoTranslations">>, 2, <<"se">>))].
+gettext6_loaded(_) ->
+    [?_assertEqual(<<"Fisk">>, ?N_(<<"se">>, <<"Fish">>, <<"Fishes">>, 1)),
+     ?_assertEqual(<<"Fiskar">>, ?N_(<<"se">>, <<"Fish">>, <<"Fishes">>, 2)),
+     ?_assertEqual(<<"NoTranslation">>, ?N_(<<"se">>, <<"NoTranslation">>, <<"NoTranslations">>, 1)),
+     ?_assertEqual(<<"NoTranslations">>, ?N_(<<"se">>, <<"NoTranslation">>, <<"NoTranslations">>, 2)),
+     ?_assertEqual(<<"Get">>, ?NP_(<<"se">>, <<"Context">>, <<"Goat">>, <<"Goats">>, 1)),
+     ?_assertEqual(<<"Getter">>, ?NP_(<<"se">>, <<"Context">>, <<"Goat">>, <<"Goats">>, 2)),
+     ?_assertEqual(<<"NoTranslation">>, ?NP_(<<"se">>, <<"Context">>, <<"NoTranslation">>, <<"NoTranslations">>, 1)),
+     ?_assertEqual(<<"NoTranslations">>, ?NP_(<<"se">>, <<"Context">>, <<"NoTranslation">>, <<"NoTranslations">>, 2))].
 
-%% Calls to dnpgettext/6 withour loaded translations
-dnpgettext_noloaded(_) ->
-    [?_assertEqual(<<"Fish">>, ?N_(<<"Fish">>, <<"Fishes">>, 1, <<"se">>)),
-     ?_assertEqual(<<"Fishes">>, ?N_(<<"Fish">>, <<"Fishes">>, 2, <<"se">>)),
-     ?_assertEqual(<<>>, ?N_(<<>>, <<"Fishes">>, 1, <<"se">>)),
-     ?_assertEqual(<<>>, ?N_(<<"Fish">>, <<>>, 2, <<"se">>)),
-     ?_assertEqual(<<"Goat">>, ?NP_(<<"Context">>, <<"Goat">>, <<"Goats">>, 1, <<"se">>)),
-     ?_assertEqual(<<"Goats">>, ?NP_(<<"Context">>, <<"Goat">>, <<"Goats">>, 2, <<"se">>)),
-     ?_assertEqual(<<>>, ?NP_(<<>>, <<>>, <<"Goats">>, 1, <<"se">>)),
-     ?_assertEqual(<<>>, ?NP_(<<>>, <<"Goat">>, <<>>, 2, <<"se">>)),
-     ?_assertEqual(<<>>, ?N_(<<>>, <<>>, 1, <<>>)),
-     ?_assertEqual(<<>>, ?N_(<<>>, <<>>, 2, <<>>)),
-     ?_assertEqual(<<>>, ?NP_(<<>>, <<>>, <<>>, 1, <<>>)),
-     ?_assertEqual(<<>>, ?NP_(<<>>, <<>>, <<>>, 2, <<>>))].
+gettext6_noloaded(_) ->
+    [?_assertEqual(<<"Fish">>, ?N_(<<"se">>, <<"Fish">>, <<"Fishes">>, 1)),
+     ?_assertEqual(<<"Fishes">>, ?N_(<<"se">>, <<"Fish">>, <<"Fishes">>, 2)),
+     ?_assertEqual(<<>>, ?N_(<<"se">>, <<>>, <<"Fishes">>, 1)),
+     ?_assertEqual(<<>>, ?N_(<<"se">>, <<"Fish">>, <<>>, 2)),
+     ?_assertEqual(<<"Goat">>, ?NP_(<<"se">>, <<"Context">>, <<"Goat">>, <<"Goats">>, 1)),
+     ?_assertEqual(<<"Goats">>, ?NP_(<<"se">>, <<"Context">>, <<"Goat">>, <<"Goats">>, 2)),
+     ?_assertEqual(<<>>, ?NP_(<<"se">>, <<>>, <<>>, <<"Goats">>, 1)),
+     ?_assertEqual(<<>>, ?NP_(<<"se">>, <<>>, <<"Goat">>, <<>>, 2)),
+     ?_assertEqual(<<>>, ?N_(<<>>, <<>>, <<>>, 1)),
+     ?_assertEqual(<<>>, ?N_(<<>>, <<>>, <<>>, 2)),
+     ?_assertEqual(<<>>, ?NP_(<<>>, <<>>, <<>>, <<>>, 1)),
+     ?_assertEqual(<<>>, ?NP_(<<>>, <<>>, <<>>, <<>>, 2))].
 
 start() ->
-    {ok, Pid} = gettexter_sup:start_link(),
-    Pid.
+    case gettexter_server:start_link() of
+         {ok, Pid} -> Pid;
+         {error, {already_started, Pid}} -> Pid
+    end.
 
 start_load() ->
-    {ok, Pid} = gettexter_sup:start_link(),
-    ok = gettexter:bindtextdomain(?GETTEXT_DOMAIN, "test/locale"),
-    {ok, _} = gettexter:ensure_loaded(?GETTEXT_DOMAIN, lc_messages, <<"se">>),
+    Pid = start(),
+    gettexter:bindtextdomain(?DOMAIN, "../test/locale"),
+    gettexter:ensure_loaded(?DOMAIN, <<"se">>),
     Pid.
 
-stop(Pid) ->
-    exit(Pid, kill).
+stop(_) ->
+    ok.
